f31b6c00d9f1d33dde0719139add5ca871f9efbc
Julia Nemell <jnemell@gmail.com>
2021-11-16 15:23:12 +0100

    workshop clock

--- clock.js
+++ clock.js
@@ -1,3 +1,5 @@
+
+/* testing
 console.log(1);
 
 setTimeout( () => {             //arrowfunction. millisekunder
@@ -13,3 +15,29 @@ setInterval( () => {
 }, 1000);
 
 console.log("ticker started");
+ */
+
+
+//const now = new Date();     //gets current timestamp when this line was executed
+
+const hourEl = document.querySelector('#hour');
+const minuteEl = document.querySelector('#minute');
+const secondsEl = document.querySelector('#second');
+
+const now = () => {
+    let date = new Date();
+    let hours = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    //console.log(hours, min, sec);
+    hourEl.innerText = `${hours}`;
+    minuteEl.innerText = `${min}`;
+    secondsEl.innerText = `${sec}`;
+};
+
+setInterval("now()", 1000);
